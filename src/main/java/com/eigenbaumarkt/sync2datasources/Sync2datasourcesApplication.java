353bce9818478f49f78bdc507e37023d20e73d5b
Mesqualito <jochen@gebsattel.rocks>
2019-09-02 17:08:52 +0200

    Startover with https://www.jackrutorial.com/2018/08/multiple-datasource-in-spring-boot.html

---
+++ src/main/java/com/eigenbaumarkt/sync2datasources/Sync2datasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.eigenbaumarkt.sync2datasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sync2datasourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sync2datasourcesApplication.class, args);
+    }
+
+}
